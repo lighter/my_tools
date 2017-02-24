c3091cadd50154fecdbe827406e8bd560a094899
willy.lai <willy.lai@astrocorp.com.tw>
2017-02-24 11:09:07 +0800

    Add search Hans file

---
+++ search_chinese_file.rb
@@ -0,0 +1,25 @@
+# 找尋檔案中有中文字的
+for i in 104..113
+    site = "SITE#{i}"
+    path = "PATH#{site}/html/js/*";
+
+    files = Dir[path + '*'].select{ |f| File.file? f }
+
+    files.each do |file|
+
+        file_lines = []
+
+        File.open(file).each_with_index do |line, index|
+            hans = line.scan(/\p{Han}/)
+
+            if hans.count != 0
+                file_lines.push(index)
+            end
+        end
+
+        unless file_lines.empty?
+            puts "file: #{file}, lines: #{file_lines.join(',')}"
+        end
+
+    end
+end
